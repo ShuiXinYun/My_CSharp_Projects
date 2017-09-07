67548de361cb9e804c5620200d864111053ab142
ShuiXinYun <inmerufu@outlook.com>
2017-09-07 18:10:49 +0800

    Control added

---
+++ PlayStation_Friend_Form_Control/PlayStation_Friend_Form_Control/PlayStation_Friend_Form_Control.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace PlayStation_Friend_Form_Control
+{
+    partial class PlayStation_Friend_Form_Control
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region 组件设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(PlayStation_Friend_Form_Control));
+            this.pictureBox_background_img = new System.Windows.Forms.PictureBox();
+            this.label_level = new System.Windows.Forms.Label();
+            this.pictureBox_star = new System.Windows.Forms.PictureBox();
+            this.label_current_gaming = new System.Windows.Forms.Label();
+            this.pictureBox_isplus = new System.Windows.Forms.PictureBox();
+            this.label_userid = new System.Windows.Forms.Label();
+            this.pictureBox_online_status = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_background_img)).BeginInit();
+            this.pictureBox_background_img.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_star)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_isplus)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_online_status)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox_background_img
+            // 
+            this.pictureBox_background_img.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox_background_img.BackgroundImage")));
+            this.pictureBox_background_img.Controls.Add(this.label_level);
+            this.pictureBox_background_img.Controls.Add(this.pictureBox_star);
+            this.pictureBox_background_img.Controls.Add(this.label_current_gaming);
+            this.pictureBox_background_img.Controls.Add(this.pictureBox_isplus);
+            this.pictureBox_background_img.Controls.Add(this.label_userid);
+            this.pictureBox_background_img.Controls.Add(this.pictureBox_online_status);
+            this.pictureBox_background_img.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox_background_img.Name = "pictureBox_background_img";
+            this.pictureBox_background_img.Size = new System.Drawing.Size(1000, 200);
+            this.pictureBox_background_img.TabIndex = 0;
+            this.pictureBox_background_img.TabStop = false;
+            // 
+            // label_level
+            // 
+            this.label_level.BackColor = System.Drawing.Color.Transparent;
+            this.label_level.Font = new System.Drawing.Font("Roboto", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_level.ForeColor = System.Drawing.Color.White;
+            this.label_level.Location = new System.Drawing.Point(930, 0);
+            this.label_level.Name = "label_level";
+            this.label_level.Size = new System.Drawing.Size(70, 50);
+            this.label_level.TabIndex = 6;
+            this.label_level.Text = "15";
+            this.label_level.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // pictureBox_star
+            // 
+            this.pictureBox_star.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox_star.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox_star.Image")));
+            this.pictureBox_star.Location = new System.Drawing.Point(880, 0);
+            this.pictureBox_star.Name = "pictureBox_star";
+            this.pictureBox_star.Size = new System.Drawing.Size(50, 50);
+            this.pictureBox_star.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox_star.TabIndex = 5;
+            this.pictureBox_star.TabStop = false;
+            // 
+            // label_current_gaming
+            // 
+            this.label_current_gaming.BackColor = System.Drawing.Color.Transparent;
+            this.label_current_gaming.Font = new System.Drawing.Font("Roboto", 20F, System.Drawing.FontStyle.Italic);
+            this.label_current_gaming.ForeColor = System.Drawing.Color.White;
+            this.label_current_gaming.Location = new System.Drawing.Point(250, 140);
+            this.label_current_gaming.Name = "label_current_gaming";
+            this.label_current_gaming.Size = new System.Drawing.Size(750, 60);
+            this.label_current_gaming.TabIndex = 4;
+            this.label_current_gaming.Text = "Uncharted: A Thief\'s End";
+            this.label_current_gaming.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // pictureBox_isplus
+            // 
+            this.pictureBox_isplus.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox_isplus.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox_isplus.Image")));
+            this.pictureBox_isplus.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox_isplus.Name = "pictureBox_isplus";
+            this.pictureBox_isplus.Size = new System.Drawing.Size(50, 50);
+            this.pictureBox_isplus.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox_isplus.TabIndex = 3;
+            this.pictureBox_isplus.TabStop = false;
+            // 
+            // label_userid
+            // 
+            this.label_userid.BackColor = System.Drawing.Color.Transparent;
+            this.label_userid.Location = new System.Drawing.Point(250, 0);
+            this.label_userid.Name = "label_userid";
+            this.label_userid.Size = new System.Drawing.Size(250, 50);
+            this.label_userid.TabIndex = 2;
+            // 
+            // pictureBox_online_status
+            // 
+            this.pictureBox_online_status.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox_online_status.Location = new System.Drawing.Point(0, 75);
+            this.pictureBox_online_status.Name = "pictureBox_online_status";
+            this.pictureBox_online_status.Size = new System.Drawing.Size(50, 50);
+            this.pictureBox_online_status.TabIndex = 1;
+            this.pictureBox_online_status.TabStop = false;
+            // 
+            // PlayStation_Friend_Form_Control
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.pictureBox_background_img);
+            this.Name = "PlayStation_Friend_Form_Control";
+            this.Size = new System.Drawing.Size(1000, 200);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_background_img)).EndInit();
+            this.pictureBox_background_img.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_star)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_isplus)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_online_status)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox_background_img;
+        private System.Windows.Forms.PictureBox pictureBox_online_status;
+        private System.Windows.Forms.Label label_userid;
+        private System.Windows.Forms.PictureBox pictureBox_isplus;
+        private System.Windows.Forms.Label label_current_gaming;
+        private System.Windows.Forms.PictureBox pictureBox_star;
+        private System.Windows.Forms.Label label_level;
+    }
+}
